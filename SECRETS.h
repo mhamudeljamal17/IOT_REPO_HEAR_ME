bfc342426970a996018eb62b35a913c2395cf6be
rulaYounis <43337059+rulaYounis@users.noreply.github.com>
2026-01-28 03:53:24 +0200

    Create SECRETS.h

---
+++ SECRETS.h
@@ -0,0 +1,20 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+/* ============== WIFI CONFIGURATION ============== */
+#define WIFI_SSID "Your_WiFi_SSID"
+#define WIFI_PASS "Your_WiFi_Password"
+
+/* ============== FIREBASE CONFIGURATION ============== */
+#define API_KEY "Your_Firebase_API_Key"
+#define STORAGE_BUCKET_ID "your-project.firebasestorage.app"
+#define FIRESTORE_PROJECT_ID "your-project-id"
+#define DATABASE_URL "https://your-project-default-rtdb.region.firebasedatabase.app/"
+#define USER_EMAIL "your_email@example.com"
+#define USER_PASSWORD "your_password"
+
+/* ============== MENTEE CONFIGURATION ============== */
+#define MENTEE_NUMBER 0000
+#define ESP_COMMAND_PATH "/esp_commands/0000"
+
+#endif // SECRETS_H
